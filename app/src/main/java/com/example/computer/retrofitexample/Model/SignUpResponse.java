bb773bc392915e0a2d9b3c78566ec3572ac62088
segunfrancis <francis4dblues@gmail.com>
2019-04-17 22:26:17 +0100

    Add SignUpResponse POJO class

---
+++ app/src/main/java/com/example/computer/retrofitexample/Model/SignUpResponse.java
@@ -0,0 +1,31 @@
+package com.example.computer.retrofitexample.Model;
+
+public class SignUpResponse {
+    private String success;
+    private String message;
+    private String userId;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
